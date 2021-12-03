732f9b2e03a1b17f6a36f0773ba57a28152f08c8
Zubayr-dev <bekzodbekolimbekov@gmail.com>
2021-12-03 14:50:53 +0500

    should work now!

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cd dist
 git init 
 git add -A
 git commit -m 'New deployment'
-git push -f  git@github.com:Zubayr-dev/Magnat-crm.git master:develop
+git push -f  git@github.com:Zubayr-dev/Magnat-crm.git main:develop
 
 cd -
 
